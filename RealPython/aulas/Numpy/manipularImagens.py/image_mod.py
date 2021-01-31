722a11759d7f1b7bcae84f39d4c6c5ce7cd5e1a4
cassio645 <51422880+cassio645@users.noreply.github.com>
2021-01-31 10:52:05 -0300

    Delete image_mod.py

--- RealPython/aulas/Numpy/manipularImagens.py/image_mod.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.image as mpimg
-
-img = mpimg.imread(r"C:\Users\CASSIO\Documents\Python\Aprendendo-python\RealPython\aulas\Numpy\manipularImagens.py\kitty.jpg")
-print(type(img))
-print(img.shape)
-
