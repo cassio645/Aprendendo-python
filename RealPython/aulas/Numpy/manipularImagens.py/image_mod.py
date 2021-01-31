d6984a986650a48cf8032ff177651ceb12dae99a
cassio645 <51422880+cassio645@users.noreply.github.com>
2021-01-31 10:31:41 -0300

    img

---
+++ RealPython/aulas/Numpy/manipularImagens.py/image_mod.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.image as mpimg
+
+img = mpimg.imread(r"C:\Users\CASSIO\Documents\Python\Aprendendo-python\RealPython\aulas\Numpy\manipularImagens.py\kitty.jpg")
+print(type(img))
+print(img.shape)
+
