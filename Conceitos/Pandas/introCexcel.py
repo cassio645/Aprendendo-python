c0d932de7612be89fb02c70e389a8e83c38998f8
cassio645 <51422880+cassio645@users.noreply.github.com>
2021-02-01 10:16:12 -0300

    trocando nome

--- Conceitos/Pandas/introCexcel.py
+++ Conceitos/Pandas/introCexcel.py
@@ -5,6 +5,6 @@ dados = pd.read_excel('C:/Users/CASSIO/Documents/Python/Aprendendo-python/Concei
 print(dados.head())
 '''
 
-dados2 = pd.read_csv('C:/Users/CASSIO/Documents/Python/Aprendendo-python/Conceitos/pandas/athlete_events.csv')
+dados2 = pd.read_csv('C:/Users/CASSIO/Documents/Python/Aprendendo-python/Conceitos/pandas/nome_do_arquivo_grande.csv')
 print(dados2.head(10))
 print(dados2.shape) # (271116, 15)
