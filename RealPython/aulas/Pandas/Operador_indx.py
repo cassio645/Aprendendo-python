f181b34c5544ff4b84918b94f45c06465cff8f0e
cassio645 <51422880+cassio645@users.noreply.github.com>
2021-03-15 11:17:24 -0300

    Delete RealPython directory

--- RealPython/aulas/Pandas/Operador_indx.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-toys = pd.DataFrame([
-    {'nome': 'bola', 'shape': 'redondo'},
-    {'nome': 'Cubo mágico', 'shape': 'cubo'}
-])
-print(toys['shape']) # redondo  cubo
-
-print(toys.shape) # (2, 2)
--- RealPython/aulas/enumerate/enumerate.py
+++
@@ -1,6 +0,0 @@
-lista = ['maçã', 'banana', 'abacaxi', 'laranja', 'limão', 'melancia', 'abacate']
-
-# Primeiro vem o indice depois os valores quando usar enumerate
-for idx, valor in enumerate(lista, start=1):
-    print(idx, valor)    
-
